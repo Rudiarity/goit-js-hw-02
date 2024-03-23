214ba4179862a432f1bd016f58fd97da54f3e812
Kyrylo_Fedechko <rudiari300@gmail.com>
2024-03-23 16:46:57 +0200

    start_hw-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,19 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return "Shipping to "+country+" will cost "+totalPrice+" credits"
+// function getShippingMessage(country, price, deliveryFee) {
+//     const totalPrice = price + deliveryFee;
+//     return "Shipping to "+country+" will cost "+totalPrice+" credits"
     
+// }
+// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+function formatMessage(message, maxLength) {
+  const LegthMassage = (massage) 
+    if (LegthMassage  <= maxLength) {
+        console.log(massage)
+    } else if ( LegthMassage  > maxLength) {
+        console.log(massage = maxLength + "...")
+    }
+    return
 }
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(formatMessage("Curabitur ligula sapien", 16));// "Curabitur ligula..."
