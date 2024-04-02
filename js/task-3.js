66cd77815403278df1b9649f4ce5819cec7d8559
Kyrylo_Fedechko <rudiari300@gmail.com>
2024-04-02 22:39:56 +0300

    hw_2

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,11 @@
-// function getElementWidth(content, padding, border) {
-//     const sizeContent = Number.parseFloat(content);
-//     const sizePadding = Number.parseFloat(padding);
-//     const sizeBorder = Number.parseFloat(border);
-//     const borderBox = sizeBorder * 4;
-//     const totalSize = sizeContent + sizePadding + borderBox;
-//     return   totalSize;
-    
-// }
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 106 не розумію чому у ТЗ вказано 101, бо якщо все додати виходть 106
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
-3
+function checkForSpam(message) {
+    const str = message.toLowerCase();
+    return str.includes("sale") || str.includes("spam");
+}
+    console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
